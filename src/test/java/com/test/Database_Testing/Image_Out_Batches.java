72c715bb091dd7597a95d00527457e7fb5d91338
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2024-06-13 15:33:51 +0530

    Image_Out_Batches.java

--- src/test/java/com/test/Database_Testing/Image_Out_Batches.java
+++ src/test/java/com/test/Database_Testing/Image_Out_Batches.java
@@ -33,7 +33,7 @@ public class Image_Out_Batches {
 
     @Parameters("slidebatchId")
     @Test
-    public void testDB(@Optional("1") String slidebatchId) {
+    public void testDB(@Optional String slidebatchId) {
         Connection connection = null;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
