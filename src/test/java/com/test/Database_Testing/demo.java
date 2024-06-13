66dfd967994266faa44542e87140617b1c0c1655
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2024-06-13 12:45:11 +0530

    demo.java

--- src/test/java/com/test/Database_Testing/demo.java
+++ src/test/java/com/test/Database_Testing/demo.java
@@ -2,82 +2,157 @@ package com.test.Database_Testing;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.LinkedHashSet;
-import java.util.Set;
 
 import org.testng.annotations.Test;
 
 public class demo {
-	
 
     @Test
-    public void testDB() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        System.out.println("Driver loaded");
+    public void testDB() {
+        Connection connection = null;
+        try {
+            // Load the MySQL JDBC driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("MySQL Driver loaded");
 
-        String url = "jdbc:mysql://apollo2.humanbrain.in:3306/HBA_V2";
-        String username = "root";
-        String password = "Health#123";
-        Connection connection = DriverManager.getConnection(url, username, password);
-        System.out.println("MYSQL database connect");
+            // Connect to the MySQL database
+            String url = "jdbc:mysql://apollo2.humanbrain.in:3306/HBA_V2";
+            String username = "root";
+            String password = "Health#123";
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println("MySQL database connected");
 
-        // Define an array of jp2Path values to loop through
-        String[] jp2Paths = {"pp1", "pp2", "pp3", "pp4", "pp5", "pp7", "pre5" ,"pre4" ,"pre7"};
+            // Do further processing here if needed
 
-        for (int i = 0; i < jp2Paths.length; i++) {
-            int count = processQuery(connection, jp2Paths[i]);
-            System.out.println("*****************************************************Total Pending Batches of " + jp2Paths[i] + ": " + count + "*****************************************************");
- 
-            // Add extra spaces between specific jp2Path values
-            if (i < jp2Paths.length - 1) {
-                System.out.println(); // Add an empty line for spacing
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL JDBC driver not found.");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Failed to connect to the database.");
+            e.printStackTrace();
+        } finally {
+            // Close the connection in finally block to ensure it's always closed
+            try {
+                if (connection != null && !connection.isClosed()) {
+                    connection.close();
+                    System.out.println("Connection closed.");
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
         }
-
-        // Close the database connection
-        connection.close();
     }
-
-    private int processQuery(Connection connection, String jp2Path) throws SQLException {
-        Statement statement = connection.createStatement();
-        String query = "SELECT " +
-                "sb.id AS slidebatch_id, " +
-                "sb.name AS name, " +
-                "s.filename AS filename " +
-                "FROM slidebatch sb " +
-                "LEFT JOIN slide s ON sb.id = s.slidebatch " +
-                "WHERE sb.process_status = 8 " +
-                "AND sb.arrival_date LIKE '%2024%'" +           
-                "AND s.jp2Path LIKE '%" + jp2Path + "%';";
-        ResultSet resultSet = statement.executeQuery(query);
-
-        Set<Integer> distinctBatchIds = new LinkedHashSet<>();
-        Integer lastId = null;  // Keep track of the last processed ID
-
-        while (resultSet.next()) {
-            Integer id = resultSet.getInt("slidebatch_id");
-            String name = resultSet.getString("name");
-            String filename = resultSet.getString("filename");
-
-            // Check if the current ID is different from the last one and insert a space
-            if (lastId != null && !id.equals(lastId)) {
-                System.out.println(); // Insert a space if not the first unique ID
-            }
-
-            System.out.println("ID: " + id + "----- Name: " + name + "--------- Filename: " + filename);
-
-            distinctBatchIds.add(id);
-            lastId = id;
-        }
-
-        // Close resources
-        resultSet.close();
-        statement.close();
-
-        return distinctBatchIds.size();
-    }
-
 }
+
+
+//            // Get slidebatch ID from user input
+//            Scanner scanner = new Scanner(System.in);
+//            System.out.println("Enter the slidebatch ID:");
+//            int slidebatchId = scanner.nextInt();
+//
+//            // Execute the query and collect results
+//            List<Map<String, Object>> queryResults = executeAndCollectQueryResults(connection, slidebatchId);
+//
+//            connection.close();
+//
+//            // Print the query results
+//            printQueryResults(queryResults);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        } finally {
+//            try {
+//                if (connection != null && !connection.isClosed()) {
+//                    connection.close();
+//                }
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+//
+//    private List<Map<String, Object>> executeAndCollectQueryResults(Connection connection, int slidebatchId) {
+//        List<Map<String, Object>> queryResults = new ArrayList<>();
+//        Statement statement = null;
+//        ResultSet resultSet = null;
+//
+//        try {
+//            statement = connection.createStatement();
+//            String query = "SELECT slidebatch.id, slide.filename, huron_slideinfo.isQC FROM slidebatch "
+//                         + "LEFT JOIN slide ON slide.slidebatch = slidebatch.id "
+//                         + "LEFT JOIN huron_slideinfo ON huron_slideinfo.slide = slide.id "
+//                         + "WHERE slidebatch.id = " + slidebatchId;
+//
+//            resultSet = statement.executeQuery(query);
+//
+//            while (resultSet.next()) {
+//                Map<String, Object> result = new HashMap<>();
+//                result.put("id", resultSet.getInt("id"));
+//                result.put("filename", resultSet.getString("filename"));
+//                result.put("isQC", resultSet.getBoolean("isQC"));
+//
+//                String filename = resultSet.getString("filename");
+//                // Extract the biosample value
+//                String biosample = filename.substring(filename.indexOf("B_") + 2,
+//                        filename.indexOf('_', filename.indexOf("B_") + 2));
+//                result.put("biosample", biosample);
+//
+//                // Extract the series value
+//                String series = filename.substring(filename.indexOf("ST_") + 3,
+//                        filename.indexOf('-', filename.indexOf("ST_") + 3));
+//                result.put("series", series);
+//
+//                // Extract the section value
+//                String section = filename.substring(filename.indexOf("SE_") + 3,
+//                        filename.lastIndexOf('.'));
+//                result.put("section", section);
+//
+//                queryResults.add(result);
+//            }
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        } finally {
+//            try {
+//                if (resultSet != null) resultSet.close();
+//                if (statement != null) statement.close();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        return queryResults;
+//    }
+//
+//    private void printQueryResults(List<Map<String, Object>> queryResults) {
+//
+//        int qcTrueCount = 0;
+//        int qcFalseCount = 0;
+//
+//        for (Map<String, Object> result : queryResults) {
+//            boolean isQC = (boolean) result.get("isQC");
+//            if (isQC) {
+//                qcTrueCount++;
+//            } else {
+//                qcFalseCount++;
+//            }
+//        }
+//        System.out.println("Total no.of sections: " + queryResults.size());
+//        System.out.println("Total no.of QC passed sections: " + qcTrueCount);
+//        System.out.println("Total no.of QC failed sections: " + qcFalseCount);
+//        System.out.println("Query Result:");
+//        System.out.println("--------------------------------------------------------------------------------------------------------");
+//        System.out.printf("%-10s | %-10s | %-10s | %-10s | %-45s | %-10s%n", "ID", "Biosample", "Series", "Section", "Filename", "isQC");
+//        System.out.println("--------------------------------------------------------------------------------------------------------");
+//        for (Map<String, Object> result : queryResults) {
+//            System.out.printf("%-10d | %-10s | %-10s | %-10s | %-45s | %-10b%n",
+//                    (int) result.get("id"),
+//                    (String) result.get("biosample"),
+//                    (String) result.get("series"),
+//                    (String) result.get("section"),
+//                    (String) result.get("filename"),
+//                    (boolean) result.get("isQC"));
+//        }
+//        System.out.println("--------------------------------------------------------------------------------------------------------");
+//    }
+        //}
